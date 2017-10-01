2cdd0e2cff343aeaf40a5d52f5fcbc8fade17635
zach charlop-powers <zach.charlop.powers@gmail.com>
2017-10-01 14:29:01 -0400

    build libs

--- recipes/indigo/build.sh
+++ recipes/indigo/build.sh
@@ -6,7 +6,9 @@ mkdir -p $PREFIX
 if [ `uname` == Darwin ]; then
     export MACOSX_DEPLOYMENT_TARGET=10.9
     python build_scripts/indigo-release-libs.py --preset=mac10.9
+    python build_scripts/indigo-release-libs.py --preset=mac10.9
     
 else
-    cp Linux-amd64/bin/* $PREFIX/bin/
+  python build_scripts/indigo-release-libs.py --preset=linux64
+  python build_scripts/indigo-release-utils.py --preset=linux64
 
