69b6fe7aae39c9fc31ec26c0653b0b7a6825d5a6
zach charlop-powers <zach.charlop.powers@gmail.com>
2017-10-09 10:35:26 -0400

    fix args to utils building script

--- recipes/indigo/build.sh
+++ recipes/indigo/build.sh
@@ -6,8 +6,8 @@ mkdir -p $PREFIX/bin
 if [ "$(uname)" == "Darwin" ]; then
   export MACOSX_DEPLOYMENT_TARGET=10.9
   python build_scripts/indigo-release-libs.py --preset=mac10.9 --find-cairo --find-pixman
-  python build_scripts/indigo-release-libs.py --preset=mac10.9 --find-cairo --find-pixman
+  python build_scripts/indigo-release-libs.py --preset=mac10.9 
 else
   python build_scripts/indigo-release-libs.py  --preset=linux64 --find-cairo --find-pixman
-  python build_scripts/indigo-release-utils.py --preset=linux64 --find-cairo --find-pixman
+  python build_scripts/indigo-release-utils.py --preset=linux64 
 fi
