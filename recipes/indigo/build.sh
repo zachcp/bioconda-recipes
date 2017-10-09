2c05cf6fcc0f5c513e6c412983f6daa1aab49de9
zach charlop-powers <zach.charlop.powers@gmail.com>
2017-10-09 10:33:10 -0400

    use system (anacona) cairo and pixman to build

--- recipes/indigo/build.sh
+++ recipes/indigo/build.sh
@@ -5,9 +5,9 @@ mkdir -p $PREFIX/bin
 
 if [ "$(uname)" == "Darwin" ]; then
   export MACOSX_DEPLOYMENT_TARGET=10.9
-  python build_scripts/indigo-release-libs.py --preset=mac10.9
-  python build_scripts/indigo-release-libs.py --preset=mac10.9
+  python build_scripts/indigo-release-libs.py --preset=mac10.9 --find-cairo --find-pixman
+  python build_scripts/indigo-release-libs.py --preset=mac10.9 --find-cairo --find-pixman
 else
-  python build_scripts/indigo-release-libs.py --preset=linux64
-  python build_scripts/indigo-release-utils.py --preset=linux64
+  python build_scripts/indigo-release-libs.py  --preset=linux64 --find-cairo --find-pixman
+  python build_scripts/indigo-release-utils.py --preset=linux64 --find-cairo --find-pixman
 fi
