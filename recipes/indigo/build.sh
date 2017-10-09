74c2f9e15d44545d52bbf90a4f2ac648cfbecca8
zach charlop-powers <zach.charlop.powers@gmail.com>
2017-10-09 11:26:57 -0400

    update indigo build flags

--- recipes/indigo/build.sh
+++ recipes/indigo/build.sh
@@ -5,9 +5,9 @@ mkdir -p $PREFIX/bin
 
 if [ "$(uname)" == "Darwin" ]; then
   export MACOSX_DEPLOYMENT_TARGET=10.9
-  python build_scripts/indigo-release-libs.py --preset=mac10.9 --find-cairo --find-pixman
+  python build_scripts/indigo-release-libs.py --preset=mac10.9 --find-cairo 
   python build_scripts/indigo-release-libs.py --preset=mac10.9 
 else
-  python build_scripts/indigo-release-libs.py  --preset=linux64 --find-cairo --find-pixman
+  python build_scripts/indigo-release-libs.py  --preset=linux64 --find-cairo 
   python build_scripts/indigo-release-utils.py --preset=linux64 
 fi
